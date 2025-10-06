accde8f809e5ef4a82c98c8591ee002d115cd9d9
RaihanK <raihank0192@gmail.com>
2025-10-06 21:40:07 +0600

    header added here

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import requests
 import smtplib
 import os
 
+header = {"User-Agent": "	Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36",
+          "Accept-Language": "en-US,en;q=0.9,bn;q=0.8"
+          }
+
 live_url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
 my_email = os.environ.get("MY_EMAIL")
 password = os.environ.get("PASSWORD")
-response = requests.get("https://appbrewery.github.io/instant_pot/")
+response = requests.get("https://appbrewery.github.io/instant_pot/", headers=header)
 web_page = response.text
 
 soup = BeautifulSoup(web_page, "html.parser")
