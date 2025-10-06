57a022e2a9cc5d0cb2ae457aa118b7da765896d6
RaihanK <raihank0192@gmail.com>
2025-10-06 11:38:13 +0600

    fteching the website page addded here

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # ----------------- Day 47 starts here ---------------
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://appbrewery.github.io/instant_pot/")
+web_page = response.text
+
+soup = BeautifulSoup(web_page, "html.parser")
+print(soup.title)
+
